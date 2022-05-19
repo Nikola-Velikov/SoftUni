a0e9eaa86b450b60ec6b20c79e4292ec24d19441
Nikola Velikov <102375181+Nikola-Velikov@users.noreply.github.com>
2022-05-19 23:05:47 +0300

    Add files via upload

---
+++ JS Advanced/Lab - Syntax, Functions and Statements/2. String Length.js
@@ -0,0 +1,6 @@
+function StringLength(Str1, Str2, Str3) {
+  let sumOftheLength = Str1.length + Str2.length + Str3.length;
+  console.log(sumOftheLength);
+  console.log(Math.floor(sumOftheLength / 3));
+}
+StringLength("chocolate", "ice cream", "cake");
